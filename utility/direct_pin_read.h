9985bad2780fd79f100715ade1126f4e20c21a96
PaulStoffregen <paul@pjrc.com>
2015-02-01 03:24:00 -0800

    Minor fix for future Teensy boards (using AVR conventions)

--- utility/direct_pin_read.h
+++ utility/direct_pin_read.h
@@ -1,7 +1,7 @@
 #ifndef direct_pin_read_h_
 #define direct_pin_read_h_
 
-#if defined(__AVR__) || defined(__MK20DX128__) || defined(__MK20DX256__)
+#if defined(__AVR__) || (defined(__arm__) && defined(CORE_TEENSY))
 
 #define IO_REG_TYPE			uint8_t
 #define PIN_TO_BASEREG(pin)             (portInputRegister(digitalPinToPort(pin)))
