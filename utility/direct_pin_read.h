9af6bbe8f299adbfdea9f34a157702873e24d1ac
Shane Burgess <acropup@users.noreply.github.com>
2016-01-14 12:23:37 -0800

    ESP8266 direct_pin_read.h definitions + workaround
    Added definitions, and workaround for current v2.0.0 of ESP8266 Arduino.
    Includes instruction on removing the workaround once v2.1.0 is released
    and this bug is fixed:
    https://github.com/esp8266/Arduino/issues/1110

--- utility/direct_pin_read.h
+++ utility/direct_pin_read.h
@@ -8,7 +8,7 @@
 #define PIN_TO_BITMASK(pin)             (digitalPinToBitMask(pin))
 #define DIRECT_PIN_READ(base, mask)     (((*(base)) & (mask)) ? 1 : 0)
 
-#elif defined(__SAM3X8E__)
+#elif defined(__SAM3X8E__)  // || defined(ESP8266)
 
 #define IO_REG_TYPE			uint32_t
 #define PIN_TO_BASEREG(pin)             (portInputRegister(digitalPinToPort(pin)))
@@ -21,6 +21,19 @@
 #define PIN_TO_BASEREG(pin)             (portModeRegister(digitalPinToPort(pin)))
 #define PIN_TO_BITMASK(pin)             (digitalPinToBitMask(pin))
 #define DIRECT_PIN_READ(base, mask)	(((*(base+4)) & (mask)) ? 1 : 0)
+
+/* ESP8266 v2.0.0 Arduino workaround for bug https://github.com/esp8266/Arduino/issues/1110
+   Once ESP8266 Arduino v2.1.0 is released, this #elif should be removed and line 11 of this
+   file should read:
+   #elif defined(__SAM3X8E__) || defined(ESP8266)
+*/
+#elif defined(ESP8266)
+
+#define IO_REG_TYPE			uint32_t
+#define PIN_TO_BASEREG(pin)             ((volatile uint32_t *)(0x60000000+(0x318))) //Bypassing erroneous preprocessor macros
+#define PIN_TO_BITMASK(pin)             (digitalPinToBitMask(pin))
+#define DIRECT_PIN_READ(base, mask)     (((*(base)) & (mask)) ? 1 : 0)
+/* End of workaround */
 
 #endif
 
