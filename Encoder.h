e05a32439ad147f57f6496b65850592a392a048f
PaulStoffregen <paul@pjrc.com>
2016-02-26 21:19:39 -0800

    Fix update for ENCODER_OPTIMIZE_INTERRUPTS fix #7

--- Encoder.h
+++ Encoder.h
@@ -181,7 +181,10 @@ public:
 	}
 */
 
-private:
+public:
+	// update() is not meant to be called from outside Encoder,
+	// but it is public to allow static interrupt routines.
+	// DO NOT call update() directly from sketches.
 	static void update(Encoder_internal_state_t *arg) {
 #if defined(__AVR__)
 		// The compiler believes this is just 1 line of code, so
@@ -290,6 +293,7 @@ private:
 		}
 #endif
 	}
+private:
 /*
 #if defined(__AVR__)
 	// TODO: this must be a no inline function
