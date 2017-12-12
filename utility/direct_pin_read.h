c491c3bd75f857f9dc8dee639c675bcd4ec89139
aster94 <enzo.gib@gmail.com>
2017-12-12 22:38:00 +0100

    Add files via upload

--- utility/direct_pin_read.h
+++ utility/direct_pin_read.h
@@ -30,14 +30,6 @@
 #define PIN_TO_BITMASK(pin)             (digitalPinToBitMask(pin))
 #define DIRECT_PIN_READ(base, mask)     (((*(base)) & (mask)) ? 1 : 0)
 
-/* ESP32  Arduino (https://github.com/espressif/arduino-esp32) */
-#elif defined(ESP32)
-
-#define IO_REG_TYPE			uint32_t
-#define PIN_TO_BASEREG(pin)             (portInputRegister(digitalPinToPort(pin)))
-#define PIN_TO_BITMASK(pin)             (digitalPinToBitMask(pin))
-#define DIRECT_PIN_READ(base, mask)     (((*(base)) & (mask)) ? 1 : 0)
-
 #elif defined(__SAMD21G18A__)
 
 #define IO_REG_TYPE                     uint32_t
