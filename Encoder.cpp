c3bc8b75452c007aceefc7e16d5fbb897f03271d
Maxim Prokhorov <prokhorov.max@outlook.com>
2021-07-05 01:03:58 +0300

    fixup! Only use section attribute in .cpp

--- Encoder.cpp
+++ Encoder.cpp
@@ -7,7 +7,7 @@
 
 Encoder_internal_state_t * Encoder::interruptArgs[];
 
-void ENCODER_ISR_ATTR Encoder::update(Encoder_internal_state_t *arg) ENCODER_ISR_ATTR {
+void ENCODER_ISR_ATTR Encoder::update(Encoder_internal_state_t *arg) {
 #if defined(__AVR__)
 	// The compiler believes this is just 1 line of code, so
 	// it will inline this function into each interrupt
@@ -118,183 +118,183 @@ void ENCODER_ISR_ATTR Encoder::update(Encoder_internal_state_t *arg) ENCODER_ISR
 
 #if defined(ENCODER_USE_INTERRUPTS) && !defined(ENCODER_OPTIMIZE_INTERRUPTS)
 	#ifdef CORE_INT0_PIN
-	void Encoder::isr0(void) ENCODER_ISR_ATTR { update(interruptArgs[0]); }
+	void ENCODER_ISR_ATTR Encoder::isr0(void) { update(interruptArgs[0]); }
 	#endif
 	#ifdef CORE_INT1_PIN
-	void Encoder::isr1(void) ENCODER_ISR_ATTR { update(interruptArgs[1]); }
+	void ENCODER_ISR_ATTR Encoder::isr1(void) { update(interruptArgs[1]); }
 	#endif
 	#ifdef CORE_INT2_PIN
-	void Encoder::isr2(void) ENCODER_ISR_ATTR { update(interruptArgs[2]); }
+	void ENCODER_ISR_ATTR Encoder::isr2(void) { update(interruptArgs[2]); }
 	#endif
 	#ifdef CORE_INT3_PIN
-	void Encoder::isr3(void) ENCODER_ISR_ATTR { update(interruptArgs[3]); }
+	void ENCODER_ISR_ATTR Encoder::isr3(void) { update(interruptArgs[3]); }
 	#endif
 	#ifdef CORE_INT4_PIN
-	void Encoder::isr4(void) ENCODER_ISR_ATTR { update(interruptArgs[4]); }
+	void ENCODER_ISR_ATTR Encoder::isr4(void) { update(interruptArgs[4]); }
 	#endif
 	#ifdef CORE_INT5_PIN
-	void Encoder::isr5(void) ENCODER_ISR_ATTR { update(interruptArgs[5]); }
+	void ENCODER_ISR_ATTR Encoder::isr5(void) { update(interruptArgs[5]); }
 	#endif
 	#ifdef CORE_INT6_PIN
-	void Encoder::isr6(void) ENCODER_ISR_ATTR { update(interruptArgs[6]); }
+	void ENCODER_ISR_ATTR Encoder::isr6(void) { update(interruptArgs[6]); }
 	#endif
 	#ifdef CORE_INT7_PIN
-	void Encoder::isr7(void) ENCODER_ISR_ATTR { update(interruptArgs[7]); }
+	void ENCODER_ISR_ATTR Encoder::isr7(void) { update(interruptArgs[7]); }
 	#endif
 	#ifdef CORE_INT8_PIN
-	void Encoder::isr8(void) ENCODER_ISR_ATTR { update(interruptArgs[8]); }
+	void ENCODER_ISR_ATTR Encoder::isr8(void) { update(interruptArgs[8]); }
 	#endif
 	#ifdef CORE_INT9_PIN
-	void Encoder::isr9(void) ENCODER_ISR_ATTR { update(interruptArgs[9]); }
+	void ENCODER_ISR_ATTR Encoder::isr9(void) { update(interruptArgs[9]); }
 	#endif
 	#ifdef CORE_INT10_PIN
-	void Encoder::isr10(void) ENCODER_ISR_ATTR { update(interruptArgs[10]); }
+	void ENCODER_ISR_ATTR Encoder::isr10(void) { update(interruptArgs[10]); }
 	#endif
 	#ifdef CORE_INT11_PIN
-	void Encoder::isr11(void) ENCODER_ISR_ATTR { update(interruptArgs[11]); }
+	void ENCODER_ISR_ATTR Encoder::isr11(void) { update(interruptArgs[11]); }
 	#endif
 	#ifdef CORE_INT12_PIN
-	void Encoder::isr12(void) ENCODER_ISR_ATTR { update(interruptArgs[12]); }
+	void ENCODER_ISR_ATTR Encoder::isr12(void) { update(interruptArgs[12]); }
 	#endif
 	#ifdef CORE_INT13_PIN
-	void Encoder::isr13(void) ENCODER_ISR_ATTR { update(interruptArgs[13]); }
+	void ENCODER_ISR_ATTR Encoder::isr13(void) { update(interruptArgs[13]); }
 	#endif
 	#ifdef CORE_INT14_PIN
-	void Encoder::isr14(void) ENCODER_ISR_ATTR { update(interruptArgs[14]); }
+	void ENCODER_ISR_ATTR Encoder::isr14(void) { update(interruptArgs[14]); }
 	#endif
 	#ifdef CORE_INT15_PIN
-	void Encoder::isr15(void) ENCODER_ISR_ATTR { update(interruptArgs[15]); }
+	void ENCODER_ISR_ATTR Encoder::isr15(void) { update(interruptArgs[15]); }
 	#endif
 	#ifdef CORE_INT16_PIN
-	void Encoder::isr16(void) ENCODER_ISR_ATTR { update(interruptArgs[16]); }
+	void ENCODER_ISR_ATTR Encoder::isr16(void) { update(interruptArgs[16]); }
 	#endif
 	#ifdef CORE_INT17_PIN
-	void Encoder::isr17(void) ENCODER_ISR_ATTR { update(interruptArgs[17]); }
+	void ENCODER_ISR_ATTR Encoder::isr17(void) { update(interruptArgs[17]); }
 	#endif
 	#ifdef CORE_INT18_PIN
-	void Encoder::isr18(void) ENCODER_ISR_ATTR { update(interruptArgs[18]); }
+	void ENCODER_ISR_ATTR Encoder::isr18(void) { update(interruptArgs[18]); }
 	#endif
 	#ifdef CORE_INT19_PIN
-	void Encoder::isr19(void) ENCODER_ISR_ATTR { update(interruptArgs[19]); }
+	void ENCODER_ISR_ATTR Encoder::isr19(void) { update(interruptArgs[19]); }
 	#endif
 	#ifdef CORE_INT20_PIN
-	void Encoder::isr20(void) ENCODER_ISR_ATTR { update(interruptArgs[20]); }
+	void ENCODER_ISR_ATTR Encoder::isr20(void) { update(interruptArgs[20]); }
 	#endif
 	#ifdef CORE_INT21_PIN
-	void Encoder::isr21(void) ENCODER_ISR_ATTR { update(interruptArgs[21]); }
+	void ENCODER_ISR_ATTR Encoder::isr21(void) { update(interruptArgs[21]); }
 	#endif
 	#ifdef CORE_INT22_PIN
-	void Encoder::isr22(void) ENCODER_ISR_ATTR { update(interruptArgs[22]); }
+	void ENCODER_ISR_ATTR Encoder::isr22(void) { update(interruptArgs[22]); }
 	#endif
 	#ifdef CORE_INT23_PIN
-	void Encoder::isr23(void) ENCODER_ISR_ATTR { update(interruptArgs[23]); }
+	void ENCODER_ISR_ATTR Encoder::isr23(void) { update(interruptArgs[23]); }
 	#endif
 	#ifdef CORE_INT24_PIN
-	void Encoder::isr24(void) ENCODER_ISR_ATTR { update(interruptArgs[24]); }
+	void ENCODER_ISR_ATTR Encoder::isr24(void) { update(interruptArgs[24]); }
 	#endif
 	#ifdef CORE_INT25_PIN
-	void Encoder::isr25(void) ENCODER_ISR_ATTR { update(interruptArgs[25]); }
+	void ENCODER_ISR_ATTR Encoder::isr25(void) { update(interruptArgs[25]); }
 	#endif
 	#ifdef CORE_INT26_PIN
-	void Encoder::isr26(void) ENCODER_ISR_ATTR { update(interruptArgs[26]); }
+	void ENCODER_ISR_ATTR Encoder::isr26(void) { update(interruptArgs[26]); }
 	#endif
 	#ifdef CORE_INT27_PIN
-	void Encoder::isr27(void) ENCODER_ISR_ATTR { update(interruptArgs[27]); }
+	void ENCODER_ISR_ATTR Encoder::isr27(void) { update(interruptArgs[27]); }
 	#endif
 	#ifdef CORE_INT28_PIN
-	void Encoder::isr28(void) ENCODER_ISR_ATTR { update(interruptArgs[28]); }
+	void ENCODER_ISR_ATTR Encoder::isr28(void) { update(interruptArgs[28]); }
 	#endif
 	#ifdef CORE_INT29_PIN
-	void Encoder::isr29(void) ENCODER_ISR_ATTR { update(interruptArgs[29]); }
+	void ENCODER_ISR_ATTR Encoder::isr29(void) { update(interruptArgs[29]); }
 	#endif
 	#ifdef CORE_INT30_PIN
-	void Encoder::isr30(void) ENCODER_ISR_ATTR { update(interruptArgs[30]); }
+	void ENCODER_ISR_ATTR Encoder::isr30(void) { update(interruptArgs[30]); }
 	#endif
 	#ifdef CORE_INT31_PIN
-	void Encoder::isr31(void) ENCODER_ISR_ATTR { update(interruptArgs[31]); }
+	void ENCODER_ISR_ATTR Encoder::isr31(void) { update(interruptArgs[31]); }
 	#endif
 	#ifdef CORE_INT32_PIN
-	void Encoder::isr32(void) ENCODER_ISR_ATTR { update(interruptArgs[32]); }
+	void ENCODER_ISR_ATTR Encoder::isr32(void) { update(interruptArgs[32]); }
 	#endif
 	#ifdef CORE_INT33_PIN
-	void Encoder::isr33(void) ENCODER_ISR_ATTR { update(interruptArgs[33]); }
+	void ENCODER_ISR_ATTR Encoder::isr33(void) { update(interruptArgs[33]); }
 	#endif
 	#ifdef CORE_INT34_PIN
-	void Encoder::isr34(void) ENCODER_ISR_ATTR { update(interruptArgs[34]); }
+	void ENCODER_ISR_ATTR Encoder::isr34(void) { update(interruptArgs[34]); }
 	#endif
 	#ifdef CORE_INT35_PIN
-	void Encoder::isr35(void) ENCODER_ISR_ATTR { update(interruptArgs[35]); }
+	void ENCODER_ISR_ATTR Encoder::isr35(void) { update(interruptArgs[35]); }
 	#endif
 	#ifdef CORE_INT36_PIN
-	void Encoder::isr36(void) ENCODER_ISR_ATTR { update(interruptArgs[36]); }
+	void ENCODER_ISR_ATTR Encoder::isr36(void) { update(interruptArgs[36]); }
 	#endif
 	#ifdef CORE_INT37_PIN
-	void Encoder::isr37(void) ENCODER_ISR_ATTR { update(interruptArgs[37]); }
+	void ENCODER_ISR_ATTR Encoder::isr37(void) { update(interruptArgs[37]); }
 	#endif
 	#ifdef CORE_INT38_PIN
-	void Encoder::isr38(void) ENCODER_ISR_ATTR { update(interruptArgs[38]); }
+	void ENCODER_ISR_ATTR Encoder::isr38(void) { update(interruptArgs[38]); }
 	#endif
 	#ifdef CORE_INT39_PIN
-	void Encoder::isr39(void) ENCODER_ISR_ATTR { update(interruptArgs[39]); }
+	void ENCODER_ISR_ATTR Encoder::isr39(void) { update(interruptArgs[39]); }
 	#endif
 	#ifdef CORE_INT40_PIN
-	void Encoder::isr40(void) ENCODER_ISR_ATTR { update(interruptArgs[40]); }
+	void ENCODER_ISR_ATTR Encoder::isr40(void) { update(interruptArgs[40]); }
 	#endif
 	#ifdef CORE_INT41_PIN
-	void Encoder::isr41(void) ENCODER_ISR_ATTR { update(interruptArgs[41]); }
+	void ENCODER_ISR_ATTR Encoder::isr41(void) { update(interruptArgs[41]); }
 	#endif
 	#ifdef CORE_INT42_PIN
-	void Encoder::isr42(void) ENCODER_ISR_ATTR { update(interruptArgs[42]); }
+	void ENCODER_ISR_ATTR Encoder::isr42(void) { update(interruptArgs[42]); }
 	#endif
 	#ifdef CORE_INT43_PIN
-	void Encoder::isr43(void) ENCODER_ISR_ATTR { update(interruptArgs[43]); }
+	void ENCODER_ISR_ATTR Encoder::isr43(void) { update(interruptArgs[43]); }
 	#endif
 	#ifdef CORE_INT44_PIN
-	void Encoder::isr44(void) ENCODER_ISR_ATTR { update(interruptArgs[44]); }
+	void ENCODER_ISR_ATTR Encoder::isr44(void) { update(interruptArgs[44]); }
 	#endif
 	#ifdef CORE_INT45_PIN
-	void Encoder::isr45(void) ENCODER_ISR_ATTR { update(interruptArgs[45]); }
+	void ENCODER_ISR_ATTR Encoder::isr45(void) { update(interruptArgs[45]); }
 	#endif
 	#ifdef CORE_INT46_PIN
-	void Encoder::isr46(void) ENCODER_ISR_ATTR { update(interruptArgs[46]); }
+	void ENCODER_ISR_ATTR Encoder::isr46(void) { update(interruptArgs[46]); }
 	#endif
 	#ifdef CORE_INT47_PIN
-	void Encoder::isr47(void) ENCODER_ISR_ATTR { update(interruptArgs[47]); }
+	void ENCODER_ISR_ATTR Encoder::isr47(void) { update(interruptArgs[47]); }
 	#endif
 	#ifdef CORE_INT48_PIN
-	void Encoder::isr48(void) ENCODER_ISR_ATTR { update(interruptArgs[48]); }
+	void ENCODER_ISR_ATTR Encoder::isr48(void) { update(interruptArgs[48]); }
 	#endif
 	#ifdef CORE_INT49_PIN
-	void Encoder::isr49(void) ENCODER_ISR_ATTR { update(interruptArgs[49]); }
+	void ENCODER_ISR_ATTR Encoder::isr49(void) { update(interruptArgs[49]); }
 	#endif
 	#ifdef CORE_INT50_PIN
-	void Encoder::isr50(void) ENCODER_ISR_ATTR { update(interruptArgs[50]); }
+	void ENCODER_ISR_ATTR Encoder::isr50(void) { update(interruptArgs[50]); }
 	#endif
 	#ifdef CORE_INT51_PIN
-	void Encoder::isr51(void) ENCODER_ISR_ATTR { update(interruptArgs[51]); }
+	void ENCODER_ISR_ATTR Encoder::isr51(void) { update(interruptArgs[51]); }
 	#endif
 	#ifdef CORE_INT52_PIN
-	void Encoder::isr52(void) ENCODER_ISR_ATTR { update(interruptArgs[52]); }
+	void ENCODER_ISR_ATTR Encoder::isr52(void) { update(interruptArgs[52]); }
 	#endif
 	#ifdef CORE_INT53_PIN
-	void Encoder::isr53(void) ENCODER_ISR_ATTR { update(interruptArgs[53]); }
+	void ENCODER_ISR_ATTR Encoder::isr53(void) { update(interruptArgs[53]); }
 	#endif
 	#ifdef CORE_INT54_PIN
-	void Encoder::isr54(void) ENCODER_ISR_ATTR { update(interruptArgs[54]); }
+	void ENCODER_ISR_ATTR Encoder::isr54(void) { update(interruptArgs[54]); }
 	#endif
 	#ifdef CORE_INT55_PIN
-	void Encoder::isr55(void) ENCODER_ISR_ATTR { update(interruptArgs[55]); }
+	void ENCODER_ISR_ATTR Encoder::isr55(void) { update(interruptArgs[55]); }
 	#endif
 	#ifdef CORE_INT56_PIN
-	void Encoder::isr56(void) ENCODER_ISR_ATTR { update(interruptArgs[56]); }
+	void ENCODER_ISR_ATTR Encoder::isr56(void) { update(interruptArgs[56]); }
 	#endif
 	#ifdef CORE_INT57_PIN
-	void Encoder::isr57(void) ENCODER_ISR_ATTR { update(interruptArgs[57]); }
+	void ENCODER_ISR_ATTR Encoder::isr57(void) { update(interruptArgs[57]); }
 	#endif
 	#ifdef CORE_INT58_PIN
-	void Encoder::isr58(void) ENCODER_ISR_ATTR { update(interruptArgs[58]); }
+	void ENCODER_ISR_ATTR Encoder::isr58(void) { update(interruptArgs[58]); }
 	#endif
 	#ifdef CORE_INT59_PIN
-	void Encoder::isr59(void) ENCODER_ISR_ATTR { update(interruptArgs[59]); }
+	void ENCODER_ISR_ATTR Encoder::isr59(void) { update(interruptArgs[59]); }
 	#endif
 #endif
