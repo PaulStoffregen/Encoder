ec8c5a1da8363cdd827bf2c5ed33f419d645c773
Mike S <CyberMerln@gmail.com>
2023-07-17 12:02:58 -0400

    update interrupt pins for wifi board

--- utility/interrupt_pins.h
+++ utility/interrupt_pins.h
@@ -385,7 +385,7 @@
   // #define CORE_INT21_PIN	A7
 
 // Arduino Uno R4
-#elif defined(ARDUINO_UNOR4_WIFI) || defined(ARDUINO_UNOR4_MINIMA)
+#elif defined(ARDUINO_UNOR4_MINIMA)
 //0, 1, 2, 3, 8, 12, 13, 15, 16, 17, 18 and 19.
   #define CORE_NUM_INTERRUPT	12
   #define CORE_INT0_PIN		0
@@ -395,6 +395,21 @@
   #define CORE_INT8_PIN		8
   #define CORE_INT12_PIN	12
   #define CORE_INT13_PIN	13
+  #define CORE_INT15_PIN	15
+  #define CORE_INT16_PIN	16
+  #define CORE_INT17_PIN	17
+  #define CORE_INT18_PIN	18
+  #define CORE_INT19_PIN	19
+#elif defined(ARDUINO_UNOR4_WIFI)
+//0, 1, 2, 3, 8, 12, 13, 15, 16, 17, 18 and 19.
+  #define CORE_NUM_INTERRUPT	12
+  #define CORE_INT0_PIN		0
+  #define CORE_INT1_PIN		1
+  #define CORE_INT2_PIN		2
+  #define CORE_INT3_PIN		3
+  #define CORE_INT8_PIN		8
+  #define CORE_INT11_PIN	11
+  #define CORE_INT12_PIN	12
   #define CORE_INT15_PIN	15
   #define CORE_INT16_PIN	16
   #define CORE_INT17_PIN	17
